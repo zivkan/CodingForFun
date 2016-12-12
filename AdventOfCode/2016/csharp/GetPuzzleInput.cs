98ae642b123f2bd3286c9ee8f5feb1591d239bff
Andy Zivkovic <zivkovic.andy@gmail.com>
2016-12-12 22:01:43 +0100

    Advent of Code 2016 C# Day 1

---
+++ AdventOfCode/2016/csharp/GetPuzzleInput.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace csharp
+{
+    internal class GetPuzzleInput
+    {
+        internal static TextReader Day(int day)
+        {
+            return new StreamReader($@"..\..\..\input\Day{day}.txt");
+        }
+    }
+}
