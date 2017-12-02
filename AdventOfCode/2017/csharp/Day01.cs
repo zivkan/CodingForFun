6f20f9c3d31405b0587fb070f485013617d8164e
Andy Zivkovic <zivkovic.andy@gmail.com>
2017-12-02 05:52:55 +0100

    Advent of Code 2017 C# Day 1

---
+++ AdventOfCode/2017/csharp/Day01.cs
@@ -0,0 +1,83 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace csharp
+{
+    public class Day01
+    {
+        private ITestOutputHelper _output;
+
+        public Day01(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("1122", 3)]
+        [InlineData("1111", 4)]
+        [InlineData("1234", 0)]
+        [InlineData("91212129", 9)]
+        public void Part1Sample(string input, int expected)
+        {
+            int result = SolveCaptcha(input, Next);
+            _output.WriteLine("{0}", result);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            string input = GetPuzzleInput.DayText(1);
+            int result = SolveCaptcha(input, Next);
+            _output.WriteLine("{0}", result);
+        }
+
+        [Theory]
+        [InlineData("1212", 6)]
+        [InlineData("1221", 0)]
+        [InlineData("123425", 4)]
+        [InlineData("123123", 12)]
+        [InlineData("12131415", 4)]
+        public void Part2Sample(string input, int expected)
+        {
+            int result = SolveCaptcha(input, Opposite);
+            _output.WriteLine("{0}", result);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            string input = GetPuzzleInput.DayText(1);
+            int result = SolveCaptcha(input, Opposite);
+            _output.WriteLine("{0}", result);
+        }
+
+        private int SolveCaptcha(string input, Func<int, int, int> compareIndex)
+        {
+            int sum = 0;
+            int length = input.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int compare = compareIndex(i, length);
+                if (input[i] == input[compare])
+                {
+                    sum += input[i] - '0';
+                }
+            }
+
+            return sum;
+        }
+
+        private int Next(int index, int length)
+        {
+            return (index + 1) % length;
+        }
+
+        private int Opposite(int index, int length)
+        {
+            return (index + (length / 2)) % length;
+        }
+    }
+}
---
+++ AdventOfCode/2017/csharp/GetPuzzleInput.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace csharp
+{
+    internal class GetPuzzleInput
+    {
+        internal static TextReader Day(int day)
+        {
+            return new StreamReader($@"..\..\..\..\input\Day{day:d2}.txt");
+        }
+
+        internal static string DayText(int day)
+        {
+            return File.ReadAllText($@"..\..\..\..\input\Day{day:d2}.txt");
+        }
+    }
+}
