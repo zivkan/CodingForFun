849ffaddc4f1f0ca11c571e4cea4ac0113dea8de
Andy Zivkovic <zivkovic.andy@gmail.com>
2020-12-02 21:08:42 -0800

    Advent of Code 2020.01 C#

---
+++ AdventOfCode/aoc.csharp.tests/2020/Day01Tests.cs
@@ -0,0 +1,34 @@
+﻿using aoc.csharp._2020;
+using System.IO;
+using Xunit;
+
+namespace aoc.csharp.tests._2020
+{
+    public class Day01Tests
+    {
+        [Fact]
+        public void Answer()
+        {
+            using var input = Input.Get(2020, 01);
+            var (part1, part2) = Day01.GetAnswer(input);
+
+            Assert.Equal("1007104", part1);
+            Assert.Equal("18847752", part2);
+        }
+
+        [Fact]
+        public void Sample()
+        {
+            var input = new StringReader(@"1721
+979
+366
+299
+675
+1456");
+            var (part1, part2) = Day01.GetAnswer(input);
+
+            Assert.Equal("514579", part1);
+            Assert.Equal("241861950", part2);
+        }
+    }
+}
