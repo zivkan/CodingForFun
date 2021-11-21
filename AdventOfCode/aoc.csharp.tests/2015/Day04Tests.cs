ebba1ad4866e793c16b48bbe000ca3e2381bdeb0
Andy Zivkovic <zivkovic.andy@gmail.com>
2021-11-20 21:16:51 -0800

    Advent of Code 2015.04 C#

---
+++ AdventOfCode/aoc.csharp.tests/2015/Day04Tests.cs
@@ -0,0 +1,27 @@
+﻿using aoc.csharp._2015;
+using Xunit;
+
+namespace aoc.csharp.tests._2015
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void Answer()
+        {
+            using var input = Input.Get(2015, 04);
+            var (part1, part2) = Day04.GetAnswer(input);
+
+            Assert.Equal("346386", part1);
+            Assert.Equal("9958218", part2);
+        }
+
+        [Theory]
+        [InlineData("abcdef", 609043)]
+        [InlineData("pqrstuv", 1048970)]
+        public void Part1Sample(string input, int expected)
+        {
+            int houses = Day04.FindLowestWithFiveZeros(input);
+            Assert.Equal(expected, houses);
+        }
+    }
+}
---
+++ AdventOfCode/aoc.csharp/2015/Day04.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace aoc.csharp._2015
+{
+    public class Day04 : ISolver
+    {
+        public (string Part1, string Part2) GetSolution(TextReader input)
+        {
+            return GetAnswer(input);
+        }
+
+        public static (string Part1, string Part2) GetAnswer(TextReader input)
+        {
+            var key = input.ReadToEnd();
+            var part1 = FindLowestWithFiveZeros(key).ToString();
+            var part2 = FindLowestWithSixZeros(key).ToString();
+            return (part1, part2);
+        }
+
+        public static int FindLowestWithFiveZeros(string key)
+        {
+            var mask = new byte[] { 0xFF, 0xFF, 0xF0 };
+            return FindLowestMatch(key, mask);
+        }
+
+        public static int FindLowestWithSixZeros(string key)
+        {
+            var mask = new byte[] { 0xFF, 0xFF, 0xFF };
+            return FindLowestMatch(key, mask);
+        }
+
+        public static int FindLowestMatch(string key, byte[] mask)
+        {
+            using (var md5 = MD5.Create())
+            {
+                for (int i = 0; ; i++)
+                {
+                    var data = key + i;
+                    var input = Encoding.UTF8.GetBytes(data);
+                    var hash = md5.ComputeHash(input);
+                    bool match = true;
+                    for (int j = 0; match && j < mask.Length; j++)
+                    {
+                        if ((hash[j] & mask[j]) != 0)
+                        {
+                            match = false;
+                        }
+                    }
+                    if (match)
+                    {
+                        return i;
+                    }
+                }
+            }
+        }
+    }
+}
