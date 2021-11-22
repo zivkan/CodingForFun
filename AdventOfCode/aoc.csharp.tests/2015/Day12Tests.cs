1e9ea6ee2dc3462c5e06406e632e354800a1d2b2
Andy Zivkovic <zivkovic.andy@gmail.com>
2021-11-22 11:19:58 -0800

    Advent of Code 2015.12 C#

---
+++ AdventOfCode/aoc.csharp.tests/2015/Day12Tests.cs
@@ -0,0 +1,47 @@
+﻿using aoc.csharp._2015;
+using System.Text;
+using Xunit;
+
+namespace aoc.csharp.tests._2015
+{
+    public class Day12Tests
+    {
+        [Fact]
+        public void Answer()
+        {
+            using var input = Input.Get(2015, 12);
+            var (part1, part2) = Day12.GetAnswer(input);
+
+            Assert.Equal("156366", part1);
+            Assert.Equal("96852", part2);
+        }
+
+        [Theory]
+        [InlineData("[1,2,3]", 6)]
+        [InlineData("{\"a\":2,\"b\":4}", 6)]
+        [InlineData("[[[3]]]", 3)]
+        [InlineData("{\"a\":{\"b\":4},\"c\":-1}", 3)]
+        [InlineData("{\"a\":[-1,1]}", 0)]
+        [InlineData("[-1,{\"a\":1}]", 0)]
+        [InlineData("[]", 0)]
+        [InlineData("{}", 0)]
+        public void Part1(string json, int expected)
+        {
+            var bytes = Encoding.UTF8.GetBytes(json);
+            var actual = Day12.Part1(bytes);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("[1,2,3]", 6)]
+        [InlineData("[1,{\"c\":\"red\",\"b\":2},3]", 4)]
+        [InlineData("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", 0)]
+        [InlineData("[1,\"red\",5]", 6)]
+        public void Part2(string json, int expected)
+        {
+            var bytes = Encoding.UTF8.GetBytes(json);
+            var actual = Day12.Part2(bytes);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AdventOfCode/aoc.csharp/2015/Day12.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace aoc.csharp._2015
+{
+    public class Day12 : ISolver
+    {
+        public (string Part1, string Part2) GetSolution(TextReader input)
+        {
+            return GetAnswer(input);
+        }
+
+        public static (string Part1, string Part2) GetAnswer(TextReader input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input.ReadToEnd());
+
+            var part1 = Part1(bytes);
+            var part2 = Part2(bytes);
+
+            return (part1.ToString(), part2.ToString());
+        }
+
+        public static int Part1(byte[] json)
+        {
+            var reader = new Utf8JsonReader(json);
+
+            int sum = 0;
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.Number)
+                {
+                    var value = reader.GetInt32();
+                    sum += value;
+                }
+            }
+
+            return sum;
+        }
+
+        public static int Part2(byte[] json)
+        {
+            var reader = new Utf8JsonReader(json);
+
+            int sum = 0;
+            Stack<int> stack = new();
+            JsonTokenType prevTokenType = JsonTokenType.None;
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.Number)
+                {
+                    var value = reader.GetInt32();
+                    sum += value;
+                }
+                else if (reader.TokenType == JsonTokenType.StartObject)
+                {
+                    stack.Push(sum);
+                    sum = 0;
+                }
+                else if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    sum += stack.Pop();
+                }
+                else if (reader.TokenType == JsonTokenType.String && prevTokenType == JsonTokenType.PropertyName)
+                {
+                    if (reader.ValueTextEquals("red"))
+                    {
+                        var depth = reader.CurrentDepth;
+                        while (depth <= reader.CurrentDepth)
+                        {
+                            reader.Read();
+                        }
+                        sum = stack.Pop();
+                    }
+                }
+
+                prevTokenType = reader.TokenType;
+            }
+
+            return sum;
+        }
+    }
+}
