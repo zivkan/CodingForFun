e620b39a99677b296c3262f40fead527623f3912
Andy Zivkovic <zivkovic.andy@gmail.com>
2021-11-22 08:14:16 -0800

    Advent of Code 2015.10 C#

---
+++ AdventOfCode/aoc.csharp.tests/2015/Day10Tests.cs
@@ -0,0 +1,31 @@
+﻿using aoc.csharp._2015;
+using System.Linq;
+using Xunit;
+
+namespace aoc.csharp.tests._2015
+{
+    public class Day10Tests
+    {
+        [Fact]
+        public void Answer()
+        {
+            using var input = Input.Get(2015, 10);
+            var (part1, part2) = Day10.GetAnswer(input);
+
+            Assert.Equal("252594", part1);
+            Assert.Equal("3579328", part2);
+        }
+
+        [Theory]
+        [InlineData("1", "11")]
+        [InlineData("11", "21")]
+        [InlineData("21", "1211")]
+        [InlineData("1211", "111221")]
+        [InlineData("111221", "312211")]
+        public void Part1(string input, string expected)
+        {
+            var actual = Day10.Game(input).First();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AdventOfCode/aoc.csharp/2015/Day10.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace aoc.csharp._2015
+{
+    public class Day10 : ISolver
+    {
+        public (string Part1, string Part2) GetSolution(TextReader input)
+        {
+            return GetAnswer(input);
+        }
+
+        public static (string Part1, string Part2) GetAnswer(TextReader input)
+        {
+            var start = input.ReadToEnd();
+
+            string part1, part2;
+
+            using (var enumerator = Game(start).GetEnumerator())
+            {
+                for (int i = 0; i < 40; i++)
+                {
+                    enumerator.MoveNext();
+                }
+                part1 = enumerator.Current;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    enumerator.MoveNext();
+                }
+                part2 = enumerator.Current;
+            }
+
+            return (part1.Length.ToString(), part2.Length.ToString());
+        }
+
+        public static IEnumerable<string> Game(string start)
+        {
+            StringBuilder sb = new StringBuilder();
+            string last = start;
+
+            static void Append(StringBuilder sb, char c, int count)
+            {
+                sb.Append(count);
+                sb.Append(c);
+            }
+
+            for (; ; )
+            {
+                sb.Clear();
+                char c = last[0];
+                int count = 1;
+
+                for (int i = 1; i < last.Length; i++)
+                {
+                    char next = last[i];
+                    if (next != c)
+                    {
+                        Append(sb, c, count);
+                        c = next;
+                        count = 1;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+
+                Append(sb, c, count);
+                last = sb.ToString();
+                yield return last;
+            }
+        }
+    }
+}
