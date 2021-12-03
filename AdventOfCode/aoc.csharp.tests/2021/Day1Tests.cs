685ce965692890310e75402b4fc28db035c7f22f
Andy Zivkovic <zivkovic.andy@gmail.com>
2021-12-03 07:10:55 -0800

    Advent of Code 2021.01 C#

---
+++ AdventOfCode/aoc.csharp.tests/2021/Day1Tests.cs
@@ -0,0 +1,40 @@
+﻿using aoc.csharp._2021;
+using System.Collections.Generic;
+using Xunit;
+
+namespace aoc.csharp.tests._2021
+{
+    public class Day01Tests
+    {
+        private readonly IReadOnlyList<int> Sample = new int[]
+        {
+                199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+        };
+
+        [Fact]
+        public void Answer()
+        {
+            using var input = Input.Get(2021, 01);
+            var (part1, part2) = Day01.GetAnswer(input);
+
+            Assert.Equal("1342", part1);
+            Assert.Equal("1378", part2);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var actual = Day01.Part1(Sample);
+            int expected = 7;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var actual = Day01.Part2(Sample);
+            int expected = 5;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AdventOfCode/aoc.csharp/2021/Day01.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc.csharp._2021
+{
+    public class Day01 : ISolver
+    {
+        public (string Part1, string Part2) GetSolution(TextReader input)
+        {
+            return GetAnswer(input);
+        }
+
+        public static (string Part1, string Part2) GetAnswer(TextReader input)
+        {
+            var depths = Input.ToList<int>(input);
+
+            var part1 = Part1(depths);
+            var part2 = Part2(depths);
+
+            return (part1.ToString(), part2.ToString());
+        }
+
+        public static int Part1(IReadOnlyList<int> input)
+        {
+            int increases = 0;
+
+            for (int i = 1; i < input.Count; i++)
+            {
+                if (input[i] > input[i-1])
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+
+        public static int Part2(IReadOnlyList<int> input)
+        {
+            int increases = 0;
+
+            for (int i = 3; i < input.Count; i++)
+            {
+                var prev = input.Skip(i - 3).Take(3).Sum();
+                var curr = input.Skip(i - 2).Take(3).Sum();
+
+                if (curr > prev)
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+    }
+}
